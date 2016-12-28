aa2ad331a2ad128ed5f84a8072c0f6fa84a09c0c
jtl-jMojica <league@FunkyMini.local>
2016-12-28 11:49:03 -0800

    hi

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,37 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot r2d2= new Robot();
+r2d2.setSpeed(2100000000);
+r2d2.penDown();
 
+for (int i = 0; i < 1; i++) {
+	r2d2.turn(90);
+	r2d2.move(500);
+	r2d2.turn(90);		
+	r2d2.move(90);	
+	r2d2.move(90);
+	r2d2.turn(90);
+	r2d2.move(500);
+	r2d2.turn(1);
+r2d2.turn(21000000);
 	
-		
-		
+	
+	
+	
+	r2d2.move(180);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 	}
 }
